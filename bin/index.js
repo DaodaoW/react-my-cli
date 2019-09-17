85e52508a36fe60343a64f695e7a0880356c9cf8
littleFFFFFF <853247919@qq.com>
2019-09-17 17:37:12 +0800

    代码修改

--- bin/index.js
+++ bin/index.js
@@ -14,8 +14,7 @@ program
       const spinner = ora('正在搭建react项目...');
       spinner.start();
       download(
-        // 'https://github.com:Vibing/react-cli#template',
-        'https://github.com/DaodaoW/react-app#template',
+        'https://github.com:DaodaoW/react-app#master',
         name,
         { clone: true },
         err => {
@@ -24,13 +23,13 @@ program
             console.log(symbols.error, chalk.red(err));
           } else {
             spinner.succeed();
-            console.log(symbols.success, chalk.green('react项目已创建完成'));
+            console.log(symbols.success, chalk.green('The React project has been created and completed'));
           }
         }
       );
     } else {
       // 错误提示项目已存在，避免覆盖原有项目
-      console.log(symbols.error, chalk.red('该名称已经存在'));
+      console.log(symbols.error, chalk.red('File name already exists'));
     }
   });
 program.parse(process.argv);
